20bba6bcc522f0daf57d999a69b5f2eb356ffc15
unknown <husseincs@gmail.com>
2024-09-21 18:52:11 +0300

    Update config file to oop

--- conn.php
+++ conn.php
@@ -1,3 +1,12 @@
 <?php
-  $conn=mysqli_connect('localhost','root','','shop');
+ define('SERVERNAME','localhost');
+ define('DATABASEUSER','root');
+ define('USERPASSWORD','');
+ define('DATABASENAME','shop');
+ 
+$conn = new mysqli(SERVERNAME,DATABASEUSER,USERPASSWORD,DATABASENAME);
+if($conn->connect_error){
+  die("error connection:".$conn->connect_error);
+}
+  // $conn=mysqli_connect('localhost','root','','shop');
 ?>
